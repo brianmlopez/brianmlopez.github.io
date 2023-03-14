3302dc13ac7916ea954380067cbfef56283a255f
Brian Lopez <brianlopez.ccs@gmail.com>
2023-03-14 18:31:06 +0800

    03-14-2023 update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 jQuery(document).ready(function ($) {
 
-    $(".menu-icon").click(function () {
+    const navbar = document.querySelector('header');
+    window.onscroll = () => {
+        if (window.scrollY > 100) {
+            navbar.classList.add('nav-active');
+        } else {
+            navbar.classList.remove('nav-active');
+        }
+    };
+
+    $(".header-menu-icon").click(function () {
+        $(".header-menu-list").fadeIn();
         $(".header-menu-list").addClass("menu-active");
+
     });
 
     $(".menu-popup-close-btn").click(function () {
-        $(".header-menu-list").removeClass("menu-active");
+        $(".header-menu-list").fadeOut();
     });
 
     $(".header-menu-list a").click(function () {
