16567d2a8f6c33b502a5f4dc0ba3331defa3c9a7
Brian Lopez <brianlopez.ccs@gmail.com>
2023-03-16 17:03:07 +0800

    03-16-23 - Animation Update on Body

--- js/script.js
+++ js/script.js
@@ -9,6 +9,20 @@ jQuery(document).ready(function ($) {
         }
     };
 
+    $('.customFadeIn').fadeIn(1000).removeClass('customFadeIn');
+        
+
+    $(window).scroll( function(){
+        $('.fadeIn').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},1000);
+            }
+        });
+    });
+        
+
     $(".header-menu-icon").click(function () {
         $(".header-menu-list").fadeIn();
         $(".header-menu-list").addClass("menu-active");
